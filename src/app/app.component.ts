7935e0c9aa341e93c122fb7c205d3bed2f36abc3
eduqg <eduardoqgomes@gmail.com>
2017-06-14 21:51:08 -0300

    Working link to users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http, Response  } from '@angular/http';
+import 'rxjs/add/operator/map';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'User Manager AEB';
+
+  private apiURL = 'http://csf.aeb.gov.br/user';
+
+  data: any = {};
+
+  constructor(private http: Http) {
+    console.log(http);
+    this.getData();
+    console.log("1");
+    this.getUsers();
+    console.log("2");
+
+  }
+
+  getData() {
+    return this.http.get(this.apiURL)
+        .map((res: Response) => res.json())
+  }
+
+  getUsers() {
+    this.getData().subscribe(data => {
+        console.log(data);
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { UserComponentComponent } from './user-component/user-component.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponentComponent
+    UserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+        {
+           path: 'user',
+           component: UserComponent
+        }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
